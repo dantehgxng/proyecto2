7393cc5776a06c836aabdd2f29142e6f628579a9
dantehgxng <91150242+dantehgxng@users.noreply.github.com>
2021-09-21 12:16:18 -0600

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,9 +1,4 @@
 <?
-//$db = mysqli_connect("qbaco.colegioiptcedigital.com", "colegioi_qbaco", "Qbaco159753")
-//or die ("No fue posible conectar con el servidor colegioiptcedigital.");
-
-//mysqli_select_db($db, "colegioi_qbaco") or die ("No es posible seleccionar qbaco.");
-
 
 $db = mysqli_connect("localhost", "root", "12345678")
 or die ("No fue posible conectar con el servidor localhost.");
